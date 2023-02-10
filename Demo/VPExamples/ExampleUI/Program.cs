90d62882aac24a6368986cdcc6c54e2c90441f7c
Natalia Khizrieva <jesusstern@gmail.com>
2023-02-10 17:23:35 +0300

    examples added

---
+++ Demo/VPExamples/ExampleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using OOPExample;
+
+namespace ExampleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Figure>();
+            list.Add(new Round("Кружок", 2));
+            list.Add(new Quadrate("Квадратик", 4));
+
+            foreach(var i in list)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
